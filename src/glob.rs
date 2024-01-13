94959306c8ba3de20953e12b68b6a2c92f77013b
David Sherret <dsherret@gmail.com>
2024-01-13 10:57:37 -0500

    More tests

--- src/glob.rs
+++ src/glob.rs
@@ -1,12 +1,14 @@
 // Copyright 2018-2024 the Deno authors. All rights reserved. MIT license.
 
-use std::path::Component;
 use std::path::Path;
 use std::path::PathBuf;
 
 use anyhow::Context;
 use indexmap::IndexMap;
 use url::Url;
+
+use crate::util::normalize_path;
+use crate::util::specifier_to_file_path;
 
 #[derive(Clone, Default, Debug, Eq, PartialEq)]
 pub struct FilePatterns {
@@ -19,7 +21,7 @@ impl FilePatterns {
     if specifier.scheme() != "file" {
       return true;
     }
-    let path = match specifier.to_file_path() {
+    let path = match specifier_to_file_path(specifier) {
       Ok(path) => path,
       Err(_) => return true,
     };
@@ -369,39 +371,6 @@ fn match_options() -> glob::MatchOptions {
   }
 }
 
-/// Normalize all intermediate components of the path (ie. remove "./" and "../" components).
-/// Similar to `fs::canonicalize()` but doesn't resolve symlinks.
-///
-/// Taken from Cargo
-/// <https://github.com/rust-lang/cargo/blob/af307a38c20a753ec60f0ad18be5abed3db3c9ac/src/cargo/util/paths.rs#L60-L85>
-fn normalize_path<P: AsRef<Path>>(path: P) -> PathBuf {
-  let mut components = path.as_ref().components().peekable();
-  let mut ret =
-    if let Some(c @ Component::Prefix(..)) = components.peek().cloned() {
-      components.next();
-      PathBuf::from(c.as_os_str())
-    } else {
-      PathBuf::new()
-    };
-
-  for component in components {
-    match component {
-      Component::Prefix(..) => unreachable!(),
-      Component::RootDir => {
-        ret.push(component.as_os_str());
-      }
-      Component::CurDir => {}
-      Component::ParentDir => {
-        ret.pop();
-      }
-      Component::Normal(c) => {
-        ret.push(c);
-      }
-    }
-  }
-  ret
-}
-
 #[cfg(test)]
 mod test {
   use pretty_assertions::assert_eq;
@@ -571,11 +540,36 @@ mod test {
   #[test]
   fn from_relative() {
     let cwd = std::env::current_dir().unwrap();
-    let pattern = PathOrPattern::from_relative(&cwd, "./**/*.ts").unwrap();
-    assert_eq!(pattern.matches_path(&cwd.join("foo.ts")), true);
-    assert_eq!(pattern.matches_path(&cwd.join("dir/foo.ts")), true);
-    assert_eq!(pattern.matches_path(&cwd.join("foo.js")), false);
-    assert_eq!(pattern.matches_path(&cwd.join("dir/foo.js")), false);
+    // leading dot slash
+    {
+      let pattern = PathOrPattern::from_relative(&cwd, "./**/*.ts").unwrap();
+      assert_eq!(pattern.matches_path(&cwd.join("foo.ts")), true);
+      assert_eq!(pattern.matches_path(&cwd.join("dir/foo.ts")), true);
+      assert_eq!(pattern.matches_path(&cwd.join("foo.js")), false);
+      assert_eq!(pattern.matches_path(&cwd.join("dir/foo.js")), false);
+    }
+    // no leading dot slash
+    {
+      let pattern = PathOrPattern::from_relative(&cwd, "**/*.ts").unwrap();
+      assert_eq!(pattern.matches_path(&cwd.join("foo.ts")), true);
+      assert_eq!(pattern.matches_path(&cwd.join("dir/foo.ts")), true);
+      assert_eq!(pattern.matches_path(&cwd.join("foo.js")), false);
+      assert_eq!(pattern.matches_path(&cwd.join("dir/foo.js")), false);
+    }
+    // exact file, leading dot slash
+    {
+      let pattern = PathOrPattern::from_relative(&cwd, "./foo.ts").unwrap();
+      assert_eq!(pattern.matches_path(&cwd.join("foo.ts")), true);
+      assert_eq!(pattern.matches_path(&cwd.join("dir/foo.ts")), false);
+      assert_eq!(pattern.matches_path(&cwd.join("foo.js")), false);
+    }
+    // exact file, no leading dot slash
+    {
+      let pattern = PathOrPattern::from_relative(&cwd, "foo.ts").unwrap();
+      assert_eq!(pattern.matches_path(&cwd.join("foo.ts")), true);
+      assert_eq!(pattern.matches_path(&cwd.join("dir/foo.ts")), false);
+      assert_eq!(pattern.matches_path(&cwd.join("foo.js")), false);
+    }
   }
 
   #[test]
